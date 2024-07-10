35118d3540e29cfb4d0197a8501d5ffd87201fea
Kim <kimgyudo93@gmail.com>
2024-07-10 12:40:40 +0900

    feature: update apikey hide manifest and .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:project_indian/controllers/user_controller.dart';
-import 'package:project_indian/pages/home_page.dart';
 import 'package:project_indian/pages/login_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,8 +14,16 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
+
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform
+  );
+
+  print("키 셋팅 ${dotenv.get("kako_login_key")}");
+
+  KakaoSdk.init(
+    nativeAppKey: dotenv.get("kako_login_key")
   );
 
   Get.put(UserController());
@@ -38,9 +47,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: prefs.getBool("loginState")!
-        ? const HomePage()
-        : const LoginPage()
+        home: const LoginPage()
+      // home: prefs.getBool("loginState")!
+      //   ? const HomePage()
+      //   : const LoginPage()
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:project_indian/services/firebase_login_service.dart';
 
+import '../services/kako_login_service.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -12,6 +14,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
 
   FirebaseLoginService loginService = FirebaseLoginService();
+  KakoLoginService kakoLoginService = KakoLoginService('61a4233fc98aac8380237aeb5e0b1ddb');
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +25,6 @@ class _LoginPageState extends State<LoginPage> {
           width: double.infinity,
           height: double.infinity,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Container(
                 width: double.infinity,
@@ -31,10 +33,37 @@ class _LoginPageState extends State<LoginPage> {
                     onPressed: () async{
                       loginService.signInWithGoogle();
                     },
-                    child: const Text("로그인")),
+                    child: const Text("Google 로그인")),
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(bottom: 36, left: 16, right: 16),
+                child: ElevatedButton(
+                    onPressed: () async{
+                      kakoLoginService.kakaoLogin();
+                    },
+                    child: const Text("Kakao 로그인")),
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(bottom: 36, left: 16, right: 16),
+                child: ElevatedButton(
+                    onPressed: () async{
+                      kakoLoginService.kakoLogout();
+                    },
+                    child: const Text("Kako 로그아웃")),
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(bottom: 36, left: 16, right: 16),
+                child: ElevatedButton(
+                    onPressed: () async{
+                      kakoLoginService.getUser();
+                    },
+                    child: const Text("Kako 유저가져오기")),
               ),
             ],
-          ),
+        ),
         ),
       ),
     );
